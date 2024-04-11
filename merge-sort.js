2bd163d80fdf0f3da2a71a0d9bd6606f548cfca7
Camelia Rasiga <rasigacamelia@gmail.com>
2024-04-11 11:24:21 +0300

    .

--- merge-sort.js
+++ merge-sort.js
@@ -1,43 +1,37 @@
 const merge = (arr1,arr2)=>{
-    let a=0,b=0,i=0;
+    let l1=arr1.length;
+    let l2=arr2.length;
+    let a=0,b=0;
     let newArr=[];
-    
-    while(arr1[a]&&arr2[b]){
+    while(a<l1 && b<l2){
       if(arr1[a]<arr2[b]){
-        newArr[i]=arr1[a];
+        newArr.push(arr1[a]);
         a++;
       }
       else{
-        newArr[i]=arr2[b];
+        newArr.push(arr2[b]);
         b++;
       }
-      i++;
     }
-    if(arr1[a]){
-      for(let k=a; k<arr1.length; k++){
-        newArr[i] = arr1[k];
-        i++;
-      }
+    while(a<l1){
+      newArr.push(arr1[a++]);
     }
-    else {
-      for (let k=b;k<arr2.length;k++) {
-        newArr[i] = arr2[k];
-        i++;
-      }
+    while(b<l2){
+      newArr.push(arr2[b++]);
     }
     return newArr;
   }
   
   
-  const mergeSort=(arr, left, right)=>{
-    if(left>=right) return arr[left];
-    let mid = Math.floor((right+left)/2);
-    let arr1 = mergeSort(arr,left,mid);
-    let arr2 = mergeSort(arr,mid+1,right);
-    return merge(arr1,arr2);
+  const mergeSort=(arr)=>{
+    if(arr.length<=1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let leftArr=mergeSort(arr.slice(0,mid));
+    let rightArr=mergeSort(arr.slice(mid));
+    return merge(leftArr,rightArr);
   }
   
-  const merged=document.querySelector("#sorted");
-  const nums=[2,1,3,-2];
-  let newArr=mergeSort(nums,0,nums.length-1);
-  merged.textContent=newArr;
+
+  const nums=[2,1,3,0,1,9,5,-2];
+  let newArr=mergeSort(nums);
+  console.log(`New Array is ${newArr}`);
